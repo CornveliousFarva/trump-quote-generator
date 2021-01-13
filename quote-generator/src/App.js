cc700b64494c85c7d38550ba6a94c9c79b5e1d6e
Greg Schuman <gcschuman@hotmail.com>
2021-01-12 18:51:40 -0800

    Updated app.js, adding the state of the quotes and the basic layout of the page.

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import quotes from './QuoteDB'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+
+  generateQuote = (arr) => {
+    let num = Math.floor(Math.random() * quotes.length)
+
+    let newQuote = quotes[num];
+
+    this.setState({
+      quote: newQuote.quote
+    })
+
+    this.shuffleQuotes(quotes)
+  }
+
+  shuffleQuotes = (arr) => {
+    return arr.sort(function() {return 0.5 - Math.random() })
+  }
+
+  render(){
+    return(
+      <div class="container">
+        <h1 className="text-center">Trump Quote Generator</h1>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ quote-generator/src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home ()  {
+    return(
+        <div class="container">
+            <h1>Trump Quote Generator</h1>
+        </div>
+    )
+}
+
+export default Home
